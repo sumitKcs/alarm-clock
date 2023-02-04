f8a712ef8172ad51ef98d148a34055edd8b1a554
Sumit Singh Rajput <itssumit258@gmail.com>
2023-02-04 10:57:14 +0530

    added digital clock

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,12 +3,18 @@ setInterval(setClock, 1000);
 const hourHand = document.querySelector("[data-hour-hand]");
 const minuteHand = document.querySelector("[data-minute-hand]");
 const secondHand = document.querySelector("[data-second-hand]");
+const hourTime = document.querySelector("[data-hour-time]");
+const minuteTime = document.querySelector("[data-minute-time]");
+const secondTime = document.querySelector("[data-second-time]");
 
 function setClock() {
   const currentDate = new Date();
   const secondsRatio = currentDate.getSeconds() / 60;
   const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
   const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+  hourTime.innerHTML = currentDate.getHours();
+  minuteTime.innerHTML = currentDate.getMinutes();
+  secondTime.innerHTML = currentDate.getSeconds();
   setRotation(secondHand, secondsRatio);
   setRotation(minuteHand, minutesRatio);
   setRotation(hourHand, hoursRatio);
